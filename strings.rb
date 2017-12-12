25c3901401fc6d51d57e5a35106c87865ebc2718
Vasilij Schneidermann <mail@vasilij.de>
2017-12-12 18:39:38 +0100

    Improve strings detection, print offsets

--- strings.rb
+++ strings.rb
@@ -2,13 +2,34 @@
 
 require_relative 'util'
 
+def matches(string, re)
+  results = []
+  pos = 0
+  loop do
+    match = string.match(re, pos)
+    break unless match
+    results.push(match)
+    pos = match.end(0)
+  end
+  results
+end
+
+def find_prints(input)
+  matches(str(input), /(\x13[[:print:]])+/).map do |match|
+    "#{pad_pc(match.begin(0))} | #{match[0].delete("\x13")}"
+  end
+end
+
 def find_strings(input)
-  str(input).scan(/((\x13[[:graph:][:space:]])+)/)
-            .map { |m| m[0].delete("\x13") }
+  matches(str(input), /[[:print:]]{4,}/).map do |match|
+    "#{pad_pc(match.begin(0))} | #{match[0]}"
+  end
 end
 
 if ARGV.length == 1
-  puts find_strings(slurp(ARGV[0]))
+  input = slurp(ARGV[0])
+  puts find_prints(input)
+  puts find_strings(input)
 else
   puts 'usage: strings.rb <in.bin>'
 end
